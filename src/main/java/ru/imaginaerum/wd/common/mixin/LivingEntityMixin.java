e3dd682ccbd5606156e02a7b693335f162abaec7
Aleks <Aleksander.zhuckov2011@gmail.com>
2024-12-25 00:15:21 +0300

    Fix cheese

--- src/main/java/ru/imaginaerum/wd/common/mixin/LivingEntityMixin.java
+++ src/main/java/ru/imaginaerum/wd/common/mixin/LivingEntityMixin.java
@@ -2,24 +2,23 @@ package ru.imaginaerum.wd.common.mixin;
 
 import net.minecraft.tags.DamageTypeTags;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.Attackable;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.level.Level;
-import net.minecraftforge.common.extensions.IForgeLivingEntity;
 import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import ru.imaginaerum.wd.common.effects.EffectsWD;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
 
-    public void hurt(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+    // Внедряем наш код в существующий метод hurt
+    @Inject(method = "hurt", at = @At("HEAD"), cancellable = true)
+    public void onHurt(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
         LivingEntity livingEntity = (LivingEntity) (Object) this;
 
         // Проверка на наличие эффекта заморозки и огненного источника урона
-        if (livingEntity.hasEffect(EffectsWD.FREEZE.get()) && source.is(DamageTypeTags.IS_PROJECTILE)) {
+        if (livingEntity.hasEffect(EffectsWD.FREEZE.get()) && source.is(DamageTypeTags.IS_FIRE)) {
             cir.setReturnValue(false);  // Блокируем урон от огня, если есть эффект сопротивления огню
         }
     }
