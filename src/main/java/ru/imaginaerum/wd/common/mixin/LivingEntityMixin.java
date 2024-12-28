b0be311d11ddbd33fc8851d6a4975c1ab36c89dd
Aleks <Aleksander.zhuckov2011@gmail.com>
2024-12-28 17:39:57 +0300

    Fixed Mixins!

--- src/main/java/ru/imaginaerum/wd/common/mixin/LivingEntityMixin.java
+++ src/main/java/ru/imaginaerum/wd/common/mixin/LivingEntityMixin.java
@@ -6,20 +6,20 @@ import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import ru.imaginaerum.wd.common.effects.EffectsWD;
+
+import java.lang.reflect.Method;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
 
-    // Внедряем наш код в существующий метод hurt
     @Inject(method = "hurt", at = @At("HEAD"), cancellable = true)
-    public void onHurt(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+    private void onHurtModifyDamage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
         LivingEntity livingEntity = (LivingEntity) (Object) this;
-
-        // Проверка на наличие эффекта заморозки и огненного источника урона
         if (livingEntity.hasEffect(EffectsWD.FREEZE.get()) && source.is(DamageTypeTags.IS_FIRE)) {
-            cir.setReturnValue(false);  // Блокируем урон от огня, если есть эффект сопротивления огню
+            cir.setReturnValue(false);  // Отменяем дальнейшую обработку урона
         }
     }
 }
